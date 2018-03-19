6f9ebf8ad440fb8d7f62134c34386bc4193a123a
Karan Kaw <Karan.Kaw@lntinfotech.com>
2018-03-19 15:56:09 +0530

    Changed

--- matplotlibExamples/matplotlib.pyplot.plot.draw.a.simple.plot.py
+++ matplotlibExamples/matplotlib.pyplot.plot.draw.a.simple.plot.py
@@ -1,21 +1,21 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[16]:
 
 
 from matplotlib import pyplot as plt
-plt.plot([0,1,2,3,4],[0,1,4,9,16])
+plt.plot([0,1,2,3,4],[0,1,4,9,16],linewidth=3.0)
 plt.show()
 
 
-# In[5]:
+# In[10]:
 
 
 import matplotlib.pyplot as plt
 plt.xlabel('I am a Xaxis')
 plt.ylabel('I am a yaxis')
-plt.plot([1,2,3,4,5],[1,2,9,16,20],'c-')
+plt.plot([1,2,3,4,5],[1,2,9,16,20],'co')
 plt.axis([0, 6, 0, 25])
 plt.show()
 
@@ -30,15 +30,13 @@ np.arange(2,10)
 type(np.arange(0,10,3))
 
 
-# In[8]:
+# In[14]:
 
 
 from matplotlib import pyplot
-pyplot.plot([0,1,2,3,4],[0,1,8,27,64],'b-')
+pyplot.plot([0,1,2,3,4],[0,1,8,27,64],'r^')
 pyplot.axis([0,10,0,100])
-pyplot.show()import numpy
-print(numpy.ndarray.__doc__)
-help(numpy.ndarray)
+pyplot.show()
 
 
 # In[9]:
