1c869a5d047be1a20c6e412aaa7d2bf0e4191357
Karan Kaw <karankaw@hotmail.com>
2018-03-19 17:32:27 +0530

    Hmm

--- numpy/numpy.series.py
+++ numpy/numpy.series.py
@@ -21,8 +21,21 @@ pandas_dates = pd.date_range(start='20181201', end='20181231')
 pandas_dates
 
 
-# In[15]:
+# In[47]:
 
 
 pd.Series([1,'a', np.nan,True])
+
+
+# In[55]:
+
+
+#See How it makes it 0 or 1 in np.ndarray because its supposed to be homogenous
+np.array([1, 5.0, 123, False])
+
+
+# In[56]:
+
+
+np.array([1, 5.0, 123, 'Hmm', False])
 
