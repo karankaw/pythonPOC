56517c72eb479a6bf71198473856148e7c197220
Karan Kaw <Karan.Kaw@lntinfotech.com>
2018-03-19 16:47:53 +0530

    Changed Files

--- numpy/numpy.series.py
+++ numpy/numpy.series.py
@@ -1,15 +1,22 @@
 
 # coding: utf-8
 
-# In[6]:
+# In[13]:
 
 
 import numpy as np
 import pandas as pd
 
-help(pd.date_range)
+#help(pd.date_range)
 
 pandas_dates = pd.date_range(start='20181201', periods=5)
+pandas_datespandas_dates = pd.date_range(start='20181201', end='20181231')
+pandas_dates
 
+
+# In[12]:
+
+
+pandas_dates = pd.date_range(start='20181201', end='20181231')
 pandas_dates
 
